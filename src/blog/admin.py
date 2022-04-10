e4be7df4171c4cf17bef96f7fed705c8a48ec6f2
dddiiirrr3 <dddiiirrr3@gmail.com>
2022-04-10 12:17:51 +0300

    delete row fields

--- src/blog/admin.py
+++ src/blog/admin.py
@@ -8,6 +8,5 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ('status', 'created', )
     search_fields = ('title', 'body', )
     prepopulated_fields = {'slug': ('title', )}
-    raw_id_fields = ('author', )
     date_hierarchy = 'publish'
     ordering = ('status', 'publish',)
