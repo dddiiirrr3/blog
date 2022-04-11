5cac1fd5b10204c853ab61e79551e3c98430622d
dddiiirrr3 <dddiiirrr3@gmail.com>
2022-04-11 18:50:31 +0300

    tags

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,14 +1,22 @@
 from django.core.mail import send_mail
+from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from taggit.models import Tag
 
-from .forms import EmailPostForm
+from .forms import EmailPostForm, CommentForm
 from .models import Post
 
 
-def post_list(request):
+def post_list(request, tag_slug=None):
     object_list = Post.published.all()
+    tag = None
+
+    if tag_slug:
+        tag = get_object_or_404(Tag, slug=tag_slug)
+        object_list = object_list.filter(tags__in=[tag])
+
     paginator = Paginator(object_list, 3)  # 3 posts in each page
     page = request.GET.get('page')
     try:
@@ -21,7 +29,8 @@ def post_list(request):
                   'blog/post/list.html',
                   {
                       'page': page,
-                      'posts': posts
+                      'posts': posts,
+                      'tag': tag,
                   })
 
 
@@ -41,10 +50,36 @@ def post_detail(request, year, month, day, post):
         publish__month=month,
         publish__day=day,
     )
+
+    comments = post.comments.filter(active=True)
+
+    new_comment = None
+
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            new_comment = comment_form.save(commit=False)
+            new_comment.post = post
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(
+        same_tags=Count('tags')
+    ).order_by('-same_tags', '-publish')[:4]
+
     return render(
         request,
         'blog/post/detail.html',
-        {'post': post}
+        {'post': post,
+         'comments': comments,
+         'new_comment': new_comment,
+         'comment_form': comment_form,
+         'similar_posts': similar_posts},
     )
 
 
