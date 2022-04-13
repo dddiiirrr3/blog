1f7920ecef79f935052fe2c8a500ee745ed5edf7
dddiiirrr3 <dddiiirrr3@gmail.com>
2022-04-13 22:15:20 +0300

    postgres stemming and ranking results

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,3 +1,4 @@
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from django.core.mail import send_mail
 from django.db.models import Count
 from django.shortcuts import render, get_object_or_404
@@ -5,7 +6,7 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from taggit.models import Tag
 
-from .forms import EmailPostForm, CommentForm
+from .forms import EmailPostForm, CommentForm, SearchForm
 from .models import Post
 
 
@@ -113,3 +114,28 @@ def post_share(request, post_id):
                       'form': form,
                       'sent': sent,
                   })
+
+
+def post_search(request):
+    form = SearchForm()
+    query = None
+    results = []
+    if 'query' in request.GET:
+        form = SearchForm(request.GET)
+        if form.is_valid():
+            query = form.cleaned_data['query']
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+            results = Post.published.annotate(
+                search=search_vector,
+                rank=SearchRank(search_vector, search_query),
+            ).filter(search=search_query).order_by('-rank')
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form': form,
+            'query': query,
+            'results': results,
+        },
+    )
--- src/mysite/settings.py
+++ src/mysite/settings.py
@@ -50,6 +50,8 @@ INSTALLED_APPS = [
     'taggit',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+
+    'django.contrib.postgres',
 ]
 
 MIDDLEWARE = [
